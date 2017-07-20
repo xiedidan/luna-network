8c7f455952cfbd030ff9ca201aefb336f321ef80
xiedidan <xiedidan@yeah.net>
2017-07-20 14:54:43 +0800

    Issue #28 - vnet v6

--- vnet/train-test.py
+++ vnet/train-test.py
@@ -5,5 +5,5 @@ sys.path.append("../common")
 from Teacher import Teacher
 
 if __name__ == "__main__":
-    teacher = Teacher("d:/project/tianchi/data/", "v1/", 4, snapshotFilename="")
+    teacher = Teacher("d:/project/tianchi/data/", "v6/", 4, snapshotFilename="")
     teacher.train()
---
+++ vnet/v6/draw-net.bat
@@ -0,0 +1,3 @@
+python d:\project\caffe-windows\python\draw_net.py --rankdir=TB d:\project\luna-network\vnet\v6\train.prototxt d:\project\luna-network\vnet\v6\train.png
+python d:\project\caffe-windows\python\draw_net.py --rankdir=TB d:\project\luna-network\vnet\v6\test.prototxt d:\project\luna-network\vnet\v6\test.png
+python d:\project\caffe-windows\python\draw_net.py --rankdir=TB d:\project\luna-network\vnet\v6\deploy.prototxt d:\project\luna-network\vnet\v6\deploy.png
